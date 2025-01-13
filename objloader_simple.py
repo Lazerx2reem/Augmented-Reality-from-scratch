2a105db0c3dfbf4b67b2e43c3a64ecdc79fb1431
Arsh Gupta <arsh121523@gmail.com>
2025-01-13 17:31:53 -0500

    Added object loader

---
+++ objloader_simple.py
@@ -0,0 +1,8 @@
+class OBJ:
+    def __init__(self, filename, swapyz=False):
+        """Loads a Wavefront OBJ file. """
+        self.vertices = []
+        self.normals = []
+        self.texcoords = []
+        self.faces = []
+        material = None
